0e51c8f5efa7801c78074b28faa87739fcadc02b
NilsSKoog <nissemanen6968@gmail.com>
2023-10-13 21:30:28 +0200

    #

--- MA4_2.py
+++ MA4_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from person import Person
+#from person import Person
 from numba import njit
 
 def cpp_main(n):
@@ -10,8 +10,21 @@ def cpp_main(n):
 def py_main(n):
 	if n <= 1:
 		return  n
-	return py_main(n-1) + py_main(n-2)
+	return py_main(n - 1) + py_main(n - 2)
+
+@njit
+def numba_main(n):
+	if n <= 1:
+		return n
+	return numba_main(n - 1) + numba_main(n - 2)
 
 
 if __name__ == '__main__':
-	print(py_main(5))
+	xaxel = [i for i in range(20,31)]
+	print(xaxel)
+	
+	"""
+	print(cpp_main(n))
+	print(py_main(n))
+	print(numba_main(n))
+	"""
