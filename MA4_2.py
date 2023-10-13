ef940509ace90617737e4f0d44319290899b7a4c
NilsSKoog <nissemanen6968@gmail.com>
2023-10-13 21:30:47 +0200

    #

--- MA4_2.py
+++ MA4_2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-#from person import Person
+from person import Person
 from numba import njit
+from time import perf_counter as pc
+from matplotlib import pyplot as plt
+
 
 def cpp_main(n):
 	Cpp = Person(n)
@@ -19,12 +22,37 @@ def numba_main(n):
 	return numba_main(n - 1) + numba_main(n - 2)
 
 
-if __name__ == '__main__':
-	xaxel = [i for i in range(20,31)]
-	print(xaxel)
+def main():
+	x = [i for i in range(20,31)]
+	cpp_y = []
+	py_y = []
+	numba_y = []
+
+	for n in x:
+		start = pc()
+		cpp_main(n)
+		end = pc()
+		cpp_y.append(end-start)
+	plt.plot(x, cpp_y, label="C++")
 	
-	"""
-	print(cpp_main(n))
-	print(py_main(n))
-	print(numba_main(n))
-	"""
+	for n in x:
+		start = pc()
+		py_main(n)
+		end = pc()
+		py_y.append(end-start)
+	plt.plot(x, py_y, label="Python")
+	
+	for n in x:
+		start = pc()
+		numba_main(n)
+		end = pc()
+		numba_y.append(end-start)
+	plt.plot(x, numba_y, label="Numba")
+	
+	plt.xlabel("n")
+	plt.ylabel("Time (s)")
+	plt.legend()
+	plt.show()
+
+if __name__ == '__main__':
+	main()
